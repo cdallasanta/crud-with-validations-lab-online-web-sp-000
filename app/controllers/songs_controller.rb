52d0838dacc9271323684a5e116970d7f180dc2b
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-17 19:00:04 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,22 +1,34 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def new
+    @song = Song.new
   end
 
   def create
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def edit
+    @song = Song.find(params[:id])
   end
 
   def update
+    @song = Song.find(params[:id])
   end
 
-  def delete
+  def destroy
+    @song = Song.find(params[:id])
+  end
+
+  private
+
+  def song_params
+    params.require(:songs)
   end
 end
